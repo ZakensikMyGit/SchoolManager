cb016d31dee55e0db82028d41c4bda925fafacb4
Artur Zakens <zakensik@gmai.com>
2025-05-28 15:30:55 +0200

    add dto

---
+++ SchoolManager.Application/ViewModels/Employee/ListEmployeeForListVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManager.Application.ViewModels.Employee
+{
+    public class ListEmployeeForListVm
+    {
+        public List<EmployeeForListVm> Employees { get; set; }
+        public int Count { get; set; }
+    }
+}
