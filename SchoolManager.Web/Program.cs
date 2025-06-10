36e810550710ecccfbae1f48676f28c9174d9b3f
Artur Zakens <zakensik@gmail.com>
2025-06-10 22:54:14 +0200

    first step _prepare views

--- SchoolManager.Web/Program.cs
+++ SchoolManager.Web/Program.cs
@@ -63,7 +63,7 @@ namespace SchoolManager.Web
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Employee}/{action=Index}/{id?}");
             app.MapRazorPages();
 
             app.Run();
