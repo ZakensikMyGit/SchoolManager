7e51115dc2b759a6c2c4689861b1245d2b8d3a9e
Artur Zakens <zakensik@gmail.com>
2025-06-15 18:19:08 +0200

    update employee

--- SchoolManager.Domain/Model/Employee.cs
+++ SchoolManager.Domain/Model/Employee.cs
@@ -9,15 +9,20 @@ namespace SchoolManager.Domain.Model
 {
     public class Employee
     {
+        public static int MaxWorkingHours = 40;
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public double WorkingHours { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
         public DateTime EmploymentDate { get; set; } = DateTime.Now;
         public bool IsActive { get; set; }
         public int PositionId { get; set; }
 
         public virtual Position Position { get; set; }
         public virtual ICollection<Education> Educations { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
