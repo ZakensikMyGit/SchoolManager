313dc132c1942c037be43bdb1c321a6e61fb41d5
Artur Zakens <zakensik@gmail.com>
2025-08-07 21:35:56 +0200

    add enums

---
+++ SchoolManager.Domain/Enums/GroupEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManager.Domain.Enums
+{
+    public enum GroupEnum
+    {
+        KOTKI,
+        KRASNALE,
+        MOTYLE,
+        SMERFY
+    }
+}
---
+++ SchoolManager.Domain/Enums/PositionEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManager.Domain.Enums
+{
+    public enum PositionEnum
+    {
+        DYREKTOR,
+        NAUCZYCIEL,
+        PERSONEL_ADMINISTRACYJNY,
+    }
+}
