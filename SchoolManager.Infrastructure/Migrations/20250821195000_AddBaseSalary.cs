3fd1c8e6961833f038a4621bec7d79914677a595
Artur Zakens <zakensik@gmail.com>
2025-08-21 22:01:11 +0200

    repaired problem. Ready

---
+++ SchoolManager.Infrastructure/Migrations/20250821195000_AddBaseSalary.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManager.Infrastructure.Migrations
+{
+#nullable disable
+
+    namespace SchoolManager.Infrastructure.Migrations
+    {
+        [DbContext(typeof(Context))]
+        [Migration("20250821195000_AddBaseSalary")]
+        public partial class AddBaseSalary : Migration
+        {
+            protected override void Up(MigrationBuilder migrationBuilder)
+            {
+                migrationBuilder.AddColumn<decimal>(
+                    name: "BaseSalary",
+                    table: "Employees",
+                    type: "numeric",
+                    nullable: true);
+            }
+
+            protected override void Down(MigrationBuilder migrationBuilder)
+            {
+                migrationBuilder.DropColumn(
+                    name: "BaseSalary",
+                    table: "Employees");
+            }
+        }
+    }
+}
